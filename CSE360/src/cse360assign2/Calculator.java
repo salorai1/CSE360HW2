2676498a715c9e08dcf8a2a0d78b72907fccb7d6
salorai1 <salorai1@asu.edu>
2019-02-24 18:39:23 -0700

    Initial version

---
+++ CSE360/src/cse360assign2/Calculator.java
@@ -0,0 +1,89 @@
+
+package cse360assign2;
+
+/**
+ * 
+ * @author		Saleh Aloraini
+ * Class ID:	314
+ * Description: Assignment 2:
+ * 				Calculator class is a simple calculator to add, subtract,
+ * 				multiply, divide to a total value. It also has a function to 
+ * 				return the total, and one to return the history of all actions.
+ * @version 	1 before making any code changes to the file
+ *
+ */
+public class Calculator 
+{
+
+	private int total;
+	
+	/**
+	 * constructor that initializes the total and the history of the class
+	 */
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	/**
+	 * A function to return the current total of the class
+	 * @return returns the total in the calculator
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	
+	/**
+	 * A function that adds a value to the total
+	 * @param value the value added to the total
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	
+	/**
+	 * A function that subtracts a value from the total
+	 * @param value the value subtracted of the total
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	
+	/**
+	 * A function that multiplies a value by the total
+	 * @param value the value multiplied by the total
+	 */
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	
+	/**
+	 * A function that divides a value over the total, and if the value entered
+	 * is zero, the total becomes 0.
+	 * @param value the value the total divided by
+	 */
+	public void divide (int value) 
+	{
+		
+	}
+	
+	
+	/**
+	 * A method that returns all the actions that happened to the total from
+	 * the beginning.
+	 * @return returns the history of all actions as a String
+	 */
+	public String getHistory () 
+	{
+		return "";
+	}
+}
